5fa325ab9efd610a9a3d910abb58d794147a24db
k-westbrook <kcheriewestbrook@gmail.com>
2019-08-16 17:28:49 -0700

    updated headers/ created results;

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ exports.handler = async function () {
     });
     await client.connect();
     let results = await client.query('WITH CTE AS (SELECT First.restaurant_name, Second.location_name,First.restaurant_neighborhood,First.photo_url,First.review,First.rating FROM public."Restaurant" AS First LEFT JOIN public."Restaurant_Location" AS Second on First.restaurant_location = Second.location_id) SELECT CTE.restaurant_name,CTE.location_name, Third.neighborhood_name,CTE.photo_url,CTE.review,CTE.rating FROM CTE LEFT JOIN public."Restaurant_Neighborhood" AS Third ON CTE.restaurant_neighborhood = Third.neighborhood_id;');
-    return results.rows;
+
+    let response =
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: results.rows,
+    };
+    return response;
 
   } catch (err) {
     return err;
