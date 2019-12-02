17fbe1b1faa85f2b24aba9bee4554e561d16c48b
k-westbrook <kcheriewestbrook@gmail.com>
2019-12-01 18:55:28 -0800

    updated to reflect lower case change in db

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ exports.handler = async function (event, context, callback) {
 
     let restaurantResultRows = await client.query('WITH CTE AS (SELECT First.restaurant_id, First.restaurant_name, Second.location_name,First.restaurant_neighborhood,First.photo_url,First.review,First.food_type,First.rating FROM public."restaurant" AS First LEFT JOIN public."restaurant_location" AS Second  ON First.restaurant_location = Second.location_id),CTE2 AS(SELECT CTE.restaurant_id, CTE.restaurant_name,CTE.location_name, Third.neighborhood_name,CTE.photo_url,CTE.review, CTE.rating, CTE.food_type FROM CTE LEFT JOIN public."restaurant_neighborhood" AS Third ON CTE.restaurant_neighborhood = Third.neighborhood_id) SELECT CTE2.restaurant_id,CTE2.restaurant_name, CTE2.location_name, CTE2.neighborhood_name, CTE2.photo_url, CTE2.review, CTE2.rating, Fourth.food_type FROM CTE2 LEFT JOIN public."restaurant_food_type" AS Fourth ON Fourth.food_type_id = CTE2.food_type;');
 
-    let foodTypeFilterResultsRows = await client.query('SELECT DISTINCT food_type FROM public."restaurant_food_yype";');
+    let foodTypeFilterResultsRows = await client.query('SELECT DISTINCT food_type FROM public."restaurant_food_type";');
     let locationTypeFilterResultsRows = await client.query('SELECT DISTINCT location_name FROM public."restaurant_location";');
 
 
